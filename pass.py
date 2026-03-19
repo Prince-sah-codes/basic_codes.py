a127557cbfd3106f0d448da227c51da33cf8e3ed
Prince-sah-codes <sahprince4554@gmail.com>
2026-03-19 21:25:15 +0530

    commit

---
+++ pass.py
@@ -0,0 +1,14 @@
+import itertools
+
+realpass = str(input("enter your pass: "))
+tryy = "1234567890"
+
+for guess in itertools.product(tryy,repeat=len(realpass)):
+    guess = ''.join(guess)
+
+   
+
+    if guess == realpass:
+        
+        print("pass cracked:",guess)
+        break
